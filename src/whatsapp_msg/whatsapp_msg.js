4bcb083567984a889a1ff915e6d37bc4f49ab23c
Suzair0318 <khansuzair1@gmail.com>
2025-03-26 04:38:28 +0000

    whastapp puppeter error resolve

--- src/whatsapp_msg/whatsapp_msg.js
+++ src/whatsapp_msg/whatsapp_msg.js
@@ -4,6 +4,9 @@ const qrcode = require("qrcode-terminal");
 // Initialize WhatsApp client with session storage
 const client = new Client({
   authStrategy: new LocalAuth(), // Saves session automatically
+  puppeteer: {
+    args: ["--no-sandbox", "--disable-setuid-sandbox"]
+  }
 });
 
 client.on("qr", (qr) => {
